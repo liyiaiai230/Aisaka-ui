32ccc8b833579656bb8324bd6a443f51dda778f6
liyi <liyiaiai230@gmail.com>
2021-03-10 15:20:27 +0800

    添加 v-model 双向绑定input

--- aisaka/src/App.vue
+++ aisaka/src/App.vue
@@ -6,7 +6,12 @@
       <a-input value="狸三"></a-input>
     </div>
     <div class="box">
-      <a-input value="狸四" error="姓名不能少于两个字"></a-input>
+      <a-input v-model="message"></a-input>
+      <p>{{ message }}</p>
+      <button @click="message+=1">+1</button>
+    </div>
+    <div class="box">
+      <a-input value="狸五" error="姓名不能少于两个字"></a-input>
     </div>
     <div class="box">
       <Button :loading="loading1" @click="loading1 = !loading1">
@@ -36,6 +41,7 @@ import Button from './button'
 import ButtonGroup from './button-group'
 import Vue from "vue";
 
+
 Vue.component('g-button-group', ButtonGroup)
 
 
@@ -45,13 +51,19 @@ export default {
       loading1: false,
       loading2: true,
       loading3: false,
+      message: 'hi',
     }
   },
   name: 'App',
   components: {
     ButtonGroup,
     Button,
-  }
+  },
+  methods: {
+    inputChange(xxxx) {
+      console.log(xxxx.target.value)
+    }
+  },
 }
 </script>
 
