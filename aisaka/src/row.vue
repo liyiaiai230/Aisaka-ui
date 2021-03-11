6088af98fffd6c129c278f8ffe13bf88154b65c6
liyi <liyiaiai230@gmail.com>
2021-03-11 14:20:13 +0800

    添加 row 的 align=left/right/center 属性

--- aisaka/src/row.vue
+++ aisaka/src/row.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="row" :style="rowStyle">
+  <div class="row" :style="rowStyle" :class="rowClass">
     <slot></slot>
   </div>
 
@@ -10,12 +10,22 @@ export default {
   props: {
     gutter: {
       type: [Number, String]
+    },
+    align: {
+      type: String,
+      validator(value) {
+        return ['left', 'right', 'center'].includes(value)
+      }
     }
   },
   computed: {
     rowStyle() {
       let {gutter} = this
       return {marginLeft: -gutter / 2 + 'px', marginRight: -gutter / 2 + 'px'}
+    },
+    rowClass() {
+      let {align} = this
+      return [align && `align-${align}`]
     }
   },
   mounted() {
