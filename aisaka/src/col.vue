c4ff89503141f2a7d28f298e1cb3b13cb78a439a
liyi <liyiaiai230@gmail.com>
2021-03-11 13:35:05 +0800

    重构 row 和 col

--- aisaka/src/col.vue
+++ aisaka/src/col.vue
@@ -1,9 +1,6 @@
 <template>
-<div class="col" :class="[ span && `col-${span}`,offset && `offset-${offset}`]"
-     :style="{paddingLeft: gutter/2+'px',paddingRight: gutter/2+'px'}">
-  <div style="border: 1px solid green; height: 100px;">
-    <slot></slot>
-  </div>
+<div class="col" :class="colClass" :style="colStyle">
+  <slot></slot>
 </div>
 </template>
 
@@ -23,13 +20,18 @@ export default {
       gutter: 0
     }
   },
-  created() {
-    console.log('col created')
+  computed: {
+    colClass() {
+      let {span, offset} = this
+      return [span && `col-${span}`, offset && `offset-${offset}`]
+    },
+    colStyle() {
+      return {
+        paddingLeft: this.gutter / 2 + 'px',
+        paddingRight: this.gutter / 2 + 'px',
+      }
+    }
   },
-  mounted() {
-    console.log('col mounted')
-  },
-
 }
 </script>
 
--- aisaka/src/row.vue
+++ aisaka/src/row.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="row" :style="{marginLeft: -gutter/2+'px', marginRight:-gutter/2+'px'}">
+  <div class="row" :style="rowStyle">
     <slot></slot>
   </div>
 
@@ -12,12 +12,13 @@ export default {
       type: [Number, String]
     }
   },
-  created() {
-    console.log('row created')
+  computed: {
+    rowStyle() {
+      let {gutter} = this
+      return {marginLeft: -gutter / 2 + 'px', marginRight: -gutter / 2 + 'px'}
+    }
   },
   mounted() {
-    console.log('row mounted')
-    console.log(this.$children)
     this.$children.forEach((vm) => {
       vm.gutter = this.gutter
     })
